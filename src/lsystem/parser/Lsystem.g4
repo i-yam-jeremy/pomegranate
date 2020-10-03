cbfe4a6792cbccc1db83676441b8683f29c82e2d
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-10-03 15:56:36 -0700

    Worked on CMake for ANTLR

---
+++ src/lsystem/parser/Lsystem.g4
@@ -0,0 +1,10 @@
+grammar Lsystem;		
+prog:	(expr NEWLINE)* ;
+expr:	expr ('*'|'/') expr
+    |	expr ('+'|'-') expr
+    |	INT
+    |	'(' expr ')'
+    ;
+NEWLINE : [\r\n]+ ;
+INT     : [0-9]+ ;
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include "antlr4-runtime/antlr4-runtime.h"
+#include "antlr4-runtime/LsystemLexer.h"
+#include "antlr4-runtime/LsystemParser.h"
+
+using namespace antlr4;
+
 int main(int argc, char** argv) {
+	std::ifstream stream;
+	stream.open("test.lsystem");
+
+	ANTLRInputStream input(stream);
+	LsystemLexer lexer(&input);
+	CommonTokenStream tokens(&lexer);
+	LsystemParser parser(&tokens);
+
 	std::cout << "Hello world" << std::endl;
 	return 0;
 }
