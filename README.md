ab5375d5df5422db1fab01c5a1882da5a0133480
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-10-06 12:13:13 -0700

    CMake working with Windows now

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pomegranate
+
+## Pre-setup
+1. Download repo at https://github.com/antlr/antlr4
+2. Cd to <path_to_repo>/runtime/Cpp
+3. Run "deploy-windows.cmd Community"
+4. Copy output DLLs and libs (found in bin/some subdirectory) to thirdparty/antlr-runtime
+5. Copy antlr4-runtime.dll to C:\Windows\System32
+6. Run "regsvr32 antlr4-runtime.dll" on command line
--- src/lsystem/parser/LsystemLoaderVisitor.h
+++ src/lsystem/parser/LsystemLoaderVisitor.h
@@ -14,8 +14,8 @@ class  LsystemLoaderVisitor : public LsystemVisitor {
 public:
 
   virtual antlrcpp::Any visitLsystem(LsystemParser::LsystemContext *ctx) override {
-	std::shared_ptr<std::vector<Command>> initiator = visitCommands(ctx->initiator);
-	return std::make_shared<Lsystem>(initiator, std::make_shared<std::vector<Rule>>());
+	//std::shared_ptr<std::vector<Command>> initiator = visitCommands(ctx->initiator);
+	return std::make_shared<Lsystem>(std::make_shared<std::vector<Command>>(), std::make_shared<std::vector<Rule>>());
   }
 
   virtual antlrcpp::Any visitLrule(LsystemParser::LruleContext *ctx) override {
@@ -28,13 +28,14 @@ public:
   }
 
   virtual antlrcpp::Any visitSym(LsystemParser::SymContext *ctx) override {
-	switch(ctx->toString()[0]) {
+	/*switch(ctx->toString()[0]) {
 		case '-':
 			return Command(value, CommandType::YAW_LEFT);
 		default:
 			std::cerr << "Error no command symbol found" << std::endl;
 			exit(1);
-	}
+	}*/
+	  return visitChildren(ctx);
   }
 
   virtual antlrcpp::Any visitStackCommand(LsystemParser::StackCommandContext *ctx) override {
