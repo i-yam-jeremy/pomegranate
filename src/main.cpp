4bfe6466ab3d5295e9a3e8a3e8562745ee0e4ec4
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-10-14 09:42:37 -0700

    GLM library included

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <glm/vec3.hpp>
 
 #include "io/io.h"
 #include "lsystem/Lsystem.h"
@@ -9,14 +10,18 @@ int main(int argc, char** argv) {
 	auto source = io::loadTextFile("C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-input.txt");
 	auto out = lsystem::compile(source);
 
+	FbxManager* sdkManager = modelPieces.begin()->second->GetScene()->GetFbxManager();
+	FbxScene* scene = FbxScene::Create(sdkManager, "");
+
 	for (auto segment : out->getSegments()) {
 		auto model = modelPieces[segment.type];
 		std::cout << "Found model: " << model->GetName() << ", " << model->GetMesh()->GetName() << std::endl;
-		model->GetScene();
+		FbxNode* node = FbxNode::Create(sdkManager, "test");
+		// TODO add mesh to new node and then add it so it gets included in the output FBX
+		scene->GetRootNode()->AddChild((FbxNode*)model->Clone());
 	}
 
-	FbxManager* sdkManager = modelPieces.begin()->second->GetScene()->GetFbxManager();
-	FbxScene* scene = FbxScene::Create(sdkManager, "");
+	std::cout << scene->GetRootNode()->GetChildCount() << std::endl;
 
 	io::exportFbx("C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-output.fbx", scene);
 
