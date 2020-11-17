5b8ff5966bca7000b1d278f981824399008aef27
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-11-17 11:39:55 -0800

    Implemented geo::createCylinder

--- src/geo/bezier.cpp
+++ src/geo/bezier.cpp
@@ -1,5 +1,10 @@
 #include "bezier.h"
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace glm;
+
 void geo::instanceBezierCurves(FbxMesh* mesh, std::shared_ptr<lsystem::Output> lsystemOut) {
 	for (const auto& segment : lsystemOut->getSegments()) {
 		geo::instanceBezierCurve(mesh, segment);
@@ -7,20 +12,28 @@ void geo::instanceBezierCurves(FbxMesh* mesh, std::shared_ptr<lsystem::Output> l
 }
 
 void geo::instanceBezierCurve(FbxMesh* mesh, const lsystem::OutputSegment& segment) {
-	createCircle(mesh, glm::vec3(0), 1, glm::vec3(0, 1, 0), 0.0f, 8);
+	createCylinder(mesh, glm::vec3(0), 1, 1, 1, glm::vec3(0, 0, 1), 0.0f, 8);
 }
 
-void geo::createCircle(FbxMesh* mesh, glm::vec3 pos, float radius, glm::vec3 dir, float axisRotAngle, int pointCount) {
+void geo::createCylinder(FbxMesh* mesh, vec3 pos, float radius1, float radius2, float length, vec3 dir, float axisRotAngle, int pointCount) {
 	int startIndex = mesh->GetControlPointsCount();
-	mesh->InitControlPoints(startIndex + pointCount);
-	for (int i = 0; i < pointCount; i++) {
-		mesh->SetControlPointAt(FbxVector4(cos(i*2.0f*3.1415926535/pointCount), sin(i * 2.0f * 3.1415926535 / pointCount), 0), startIndex+i);
-	}
+	createCircle(mesh, pos, radius1, dir, axisRotAngle, pointCount);
+	createCircle(mesh, pos + length*dir, radius2, dir, axisRotAngle, pointCount);
 	for (int i = 0; i < pointCount; i++) {
 		mesh->BeginPolygon();
-		mesh->AddPolygon(startIndex+0);
-		mesh->AddPolygon(startIndex+i);
+		mesh->AddPolygon(startIndex + i);
 		mesh->AddPolygon(startIndex + ((i + 1) % pointCount));
+		mesh->AddPolygon(startIndex + pointCount + ((i + 1) % pointCount));
+		mesh->AddPolygon(startIndex + pointCount + i);
 		mesh->EndPolygon();
+	}
+}
+
+void geo::createCircle(FbxMesh* mesh, vec3 pos, float radius, vec3 dir, float axisRotAngle, int pointCount) {
+	int startIndex = mesh->GetControlPointsCount();
+	for (int i = 0; i < pointCount; i++) {
+		vec3 circleOffset = radius*vec3(cos(i * 2.0f * M_PI / pointCount), sin(i * 2.0f * M_PI / pointCount), 0);
+		vec3 p = pos + circleOffset;
+		mesh->SetControlPointAt(FbxVector4(p.x, p.y, p.z), startIndex+i);
 	}
 }
