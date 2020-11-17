f96f78ad87c281b4abccd6142d09bca671f1b55a
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-11-17 11:11:50 -0800

    Generated simple circle geometry

--- src/geo/bezier.cpp
+++ src/geo/bezier.cpp
@@ -7,5 +7,20 @@ void geo::instanceBezierCurves(FbxMesh* mesh, std::shared_ptr<lsystem::Output> l
 }
 
 void geo::instanceBezierCurve(FbxMesh* mesh, const lsystem::OutputSegment& segment) {
-	std::cout << "Instancing Curve: TODO" << std::endl;
+	createCircle(mesh, glm::vec3(0), 1, glm::vec3(0, 1, 0), 0.0f, 8);
+}
+
+void geo::createCircle(FbxMesh* mesh, glm::vec3 pos, float radius, glm::vec3 dir, float axisRotAngle, int pointCount) {
+	int startIndex = mesh->GetControlPointsCount();
+	mesh->InitControlPoints(startIndex + pointCount);
+	for (int i = 0; i < pointCount; i++) {
+		mesh->SetControlPointAt(FbxVector4(cos(i*2.0f*3.1415926535/pointCount), sin(i * 2.0f * 3.1415926535 / pointCount), 0), startIndex+i);
+	}
+	for (int i = 0; i < pointCount; i++) {
+		mesh->BeginPolygon();
+		mesh->AddPolygon(startIndex+0);
+		mesh->AddPolygon(startIndex+i);
+		mesh->AddPolygon(startIndex + ((i + 1) % pointCount));
+		mesh->EndPolygon();
+	}
 }
--- src/geo/bezier.h
+++ src/geo/bezier.h
@@ -2,8 +2,10 @@
 
 #include <fbxsdk.h>
 #include "../lsystem/Lsystem.h"
+#include <glm/vec3.hpp>
 
 namespace geo {
 	void instanceBezierCurves(FbxMesh* mesh, std::shared_ptr<lsystem::Output> lsystemOut);
 	void instanceBezierCurve(FbxMesh* mesh, const lsystem::OutputSegment& segment);
+	void createCircle(FbxMesh* mesh, glm::vec3 pos, float radius, glm::vec3 dir, float axisRotAngle, int pointCount);
 };
