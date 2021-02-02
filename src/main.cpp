eb975d1756923b9ff5d91736f31a2186278f9ca3
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2021-02-02 11:57:06 -0800

    Set up command line args

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,19 @@
 #include "geo/geo.h"
 
 int main(int argc, char** argv) {
-	auto source = io::loadTextFile("C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-input.txt");
+	if (argc != 2) {
+		std::cerr << "Usage: pomegranate input_file" << std::endl;
+		exit(1);
+	}
+	std::string inputFile = argv[1];
+	auto source = io::loadTextFile(inputFile);
 	
 	auto out = lsystem::compile(source);
 
 	auto mesh = geo::convertLsystemToGeo(out);
 
-	io::exportCSVRig("C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-output-rig.csv", out);
-	io::exportMesh("C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-output.obj", mesh);
+	io::exportCSVRig(inputFile + "-rig.csv", out);
+	io::exportMesh(inputFile + ".obj", mesh);
 
 	return 0;
 }
