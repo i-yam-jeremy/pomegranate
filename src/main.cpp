c7d5be04e7cc2354997d4d7470aae3a6f3183feb
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2021-02-05 15:26:15 -0800

    Used flags.h for command-line argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,41 @@
 #include <string>
 #include <chrono>
 
+#include <flags.h>
+
 #include "io/io.h"
 #include "lsystem/Lsystem.h"
 #include "geo/geo.h"
 #include "value/Random.h"
 
+/*
+	Overload for parsing std::string with flags.h argument parser
+*/
+std::istream& operator>>(std::istream& stream, std::string& s) {
+	return stream >> s;
+}
+
 int main(int argc, char** argv) {
 	argc = 2;
 	argv[1] = "C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-input.txt";
-	argv[2] = "11";
+	argv[2] = "--seed=11";
 
-	if (!(2 <= argc && argc <= 4)) {
-		std::cerr << "Usage: pomegranate input_file [seed [output_file_basename]]" << std::endl;
+	const flags::args args(argc, argv);
+	if (args.positional().size() != 1) {
+		std::cerr << "Usage: pomegranate [--basename=<base-path-to-output-files>] [--seed=<number>] input_file" << std::endl;
 		exit(1);
 	}
-	std::string inputFile = argv[1];
-	auto source = io::loadTextFile(inputFile);
+	const auto& inputFile = args.positional()[0];
 
 	auto now = std::chrono::system_clock::now().time_since_epoch().count();
-	unsigned long seed = (argc == 2) ? now : std::stol(argv[2]);
-	Random::generator.seed(seed);
+	const auto seed = args.get<long>("seed", (long)now);
+	const auto& outputFile = args.get<std::string>("basename", inputFile.data());
 
-	std::string outputFile;
-	if (argc == 4) {
-		outputFile = argv[3];
-	}
-	else {
-		outputFile = inputFile;
-	}
+	auto source = io::loadTextFile(inputFile.data());
+
+	Random::generator.seed(seed);
 	
 	auto out = lsystem::compile(source);
-
 	auto mesh = geo::convertLsystemToGeo(out);
 
 	io::exportCSVRig(outputFile + "-rig.csv", out);
