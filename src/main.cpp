9eb8b12b9a53ce1cbddc07e22c123dae4db2ebd4
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2021-02-05 08:42:21 -0800

    Command-line random seed is now optional

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <chrono>
 
 #include "io/io.h"
 #include "lsystem/Lsystem.h"
@@ -8,18 +9,19 @@
 #include "value/Random.h"
 
 int main(int argc, char** argv) {
-	argc = 3;
+	argc = 2;
 	argv[1] = "C:/Users/Jeremy Berchtold/Documents/GitHub/pomegranate/examples/test-input.txt";
 	argv[2] = "11";
 
-	if (argc != 3 && argc != 4) {
-		std::cerr << "Usage: pomegranate input_file seed [output_file_basename]" << std::endl;
+	if (!(2 <= argc && argc <= 4)) {
+		std::cerr << "Usage: pomegranate input_file [seed] [output_file_basename]" << std::endl;
 		exit(1);
 	}
 	std::string inputFile = argv[1];
 	auto source = io::loadTextFile(inputFile);
 
-	unsigned long seed = std::stol(argv[2]);
+	auto now = std::chrono::system_clock::now().time_since_epoch().count();
+	unsigned long seed = (argc == 2) ? now : std::stol(argv[2]);
 	Random::generator.seed(seed);
 
 	std::string outputFile;
