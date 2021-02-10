f6c6b8023a4b56a16f869461f3b32a9050a2d3d8
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2021-02-10 09:04:35 -0800

    Starting on custom mesh editing library

---
+++ src/geo/meshlib.cpp
@@ -0,0 +1,50 @@
+#include "meshlib.h"
+
+meshlib::Vertex meshlib::Mesh::addVertex(vec3 p) {
+	VertexData data{p};
+	size_t handle = getNextHandle();
+	vertices[handle] = data;
+	return Vertex(*this, handle);
+}
+
+meshlib::Face meshlib::Mesh::addFace(std::vector<Vertex>& verts) {
+	FaceData data;
+	for (auto& v : verts) {
+		data.vertices.push_back(getHandle(v));
+	}
+	size_t handle = getNextHandle();
+	faces[handle] = data;
+	return Face(*this, handle);
+}
+
+void meshlib::Mesh::toOBJ(std::ostream& out) {
+	std::unordered_map<Handle, size_t> vertexIndices;
+	size_t currentVertexIndex = 0;
+	for (const auto& entry : vertices) {
+		const auto& v = entry.second;
+		out << "v " << v.position.x << " " << v.position.y << " " << v.position.z << std::endl;
+		vertexIndices[entry.first] = currentVertexIndex;
+		currentVertexIndex++;
+	}
+
+	for (const auto& face : faces) {
+		out << "f ";
+		for (const auto& v : face.second.vertices) {
+			const auto vertexIndex = vertexIndices[v];
+			out << vertexIndex << " ";
+		}
+		out << std::endl;
+	}
+}
+
+meshlib::Handle meshlib::Mesh::getNextHandle() {
+	return currentHandle++;
+}
+
+meshlib::Handle meshlib::Mesh::getHandle(Vertex& v) {
+	return v.handle;
+}
+
+meshlib::Handle meshlib::Mesh::getHandle(Face& f) {
+	return f.handle;
+}
---
+++ src/geo/meshlib.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+#include <glm/vec3.hpp>
+#include <ostream>
+
+using namespace glm;
+
+namespace meshlib {
+	typedef size_t Handle;
+	
+	class Vertex;
+	class Face;
+
+	class Mesh {
+	public:
+		Mesh() {};
+		Vertex addVertex(vec3 p);
+		Face addFace(std::vector<Vertex>& verts);
+		void toOBJ(std::ostream& out);
+		Handle getHandle(Vertex& v);
+		Handle getHandle(Face& f);
+	private:
+		Handle getNextHandle();
+
+		Handle currentHandle = 0;
+		struct VertexData {
+			vec3 position;
+		};
+		struct FaceData {
+			std::vector<Handle> vertices;
+		};
+		std::unordered_map<Handle, VertexData> vertices;
+		std::unordered_map<Handle, FaceData> faces;
+	};
+
+	class Vertex {
+	public:
+		Vertex(Mesh& mesh, Handle handle) :
+			mesh(mesh),
+			handle(handle) {};
+		friend Handle Mesh::getHandle(Vertex& v);
+	private:
+		Mesh& mesh;
+		Handle handle;
+	};
+
+	class Face {
+	public:
+		Face(Mesh& mesh, Handle handle) :
+			mesh(mesh),
+			handle(handle) {};
+		friend Handle Mesh::getHandle(Face& v);
+	private:
+		Mesh& mesh;
+		Handle handle;
+	};
+}
