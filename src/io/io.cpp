bc2fe0e2348598a25bd8e3cb032a90d9aa0ceb9d
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2021-02-27 09:41:17 -0800

    Moved rig points to joints and added rig points at the start of segments with no parent

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -6,8 +6,22 @@
 void io::exportCSVRig(std::string path, std::shared_ptr<lsystem::Output> out) {
 	std::ofstream fout(path);
 	for (const auto& segment : out->getMainOut()->getSegments()) {
-		int parentId = (segment->parent == nullptr) ? -1 : segment->parent->id;
-		auto t = segment->translation;
+		int parentId;
+		if (segment->parent == nullptr) {
+			// Add a handle at the start of this segment if there is no parent
+			parentId = -1;
+			int id = out->getMainOut()->getSegments().size() + segment->id;
+			auto t = segment->translation;
+			fout << id << ",";
+			fout << parentId << ",";
+			fout << t.x << "," << t.y << "," << t.z;
+			fout << std::endl;
+			parentId = id;
+		}
+		else {
+			parentId = segment->parent->id;
+		}
+		auto t = segment->translation + vec3(vec4(1, 0, 0, 1) * segment->mat);
 		fout << segment->id << ",";
 		fout << parentId << ",";
 		fout << t.x << "," << t.y << "," << t.z;
